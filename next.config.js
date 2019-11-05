3fd8c7965cae2debbe283e965d5d277362e2a8aa
Nour Essoussi <nourcherifsoussi@gmail.com>
2019-11-05 16:13:42 -0500

    Security

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@ const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const path = require('path');
 
-// const env = process.env.NODE_ENV || 'development';
-// const stripeKey = process.env.STRIPE_KEY || 'STRIPE_KEY_HERE';
-
 module.exports = withCSS(withSass({
-//   publicRuntimeConfig: {
-//     NODE_ENV: env,
-//     STRIPE_KEY: stripeKey,
-//   },
   webpack: (config) => {
     const alias = {
       ...config.resolve.alias,
